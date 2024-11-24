f18e8cc9f915eabad5e0424c77b50a90ad4aded8
Shawn070203 <xiaochengwolong@qq.com>
2024-11-24 15:48:15 +0000

    update QLearning with better hyperparameters

--- QLearning.py
+++ QLearning.py
@@ -4,42 +4,40 @@ import matplotlib.pyplot as plt
 
 
 # Initialise the environment
-env = gym.make("FrozenLake-v1", map_name = "4x4" , is_slippery = False, render_mode=None)
+env = gym.make("FrozenLake-v1", map_name = "8x8" , is_slippery = False, render_mode=None)
 
 # Reset the environment to generate the first observation
-observation, info = env.reset(seed=42)
+# observation, info = env.reset(seed=46)
 q = np.zeros([env.observation_space.n, env.action_space.n])
-alpha = 0.8
-gamma = 0.5
+alpha = 0.9
+gamma = 0.9
 
 max_epislon = 1
 min_epislon = 0.001
 decay_rate = 0.001
 
-constant_epislon = 0.2
+constant_epislon = 0.1
 
 
-episodes_number = 2000
+episodes_number = 20000
 rewards_per_episode = np.zeros(episodes_number)
 
 for episodes in range(episodes_number):
-    t = -1
     terminated = False
     truncated = False
     state = env.reset()[0]
     while (not terminated and not truncated):
-        t += 1
         rand = np.random.rand()
         actions = np.array([0, 1, 2, 3])
         # epislon = constant_epislon
         epislon = min_epislon + (max_epislon - min_epislon)*np.exp(-decay_rate*episodes)
         if rand < 1 - epislon:
-            action = np.argmax(q[state])
+            action = np.argmax(q[state,:])
         else:
             action = np.random.choice(actions)
 
         new_state, reward, terminated, truncated, info = env.step(action)
-        q[state, action] += alpha*(reward + gamma*np.max(q[new_state]) - q[state, action])
+        q[state, action] += alpha*(reward + gamma*np.max(q[new_state,:]) - q[state, action])
         state = new_state
     rewards_per_episode[episodes] = reward
 
@@ -54,13 +52,13 @@ print('over')
 
 
 # env = gym.make("FrozenLake-v1", map_name = "8x8" , is_slippery = False, render_mode="human")
-# observation, info = env.reset(seed=35)
+# observation, info = env.reset(seed=30)
 # while True:
 #     state = 0
 #     terminated = False
 #     truncated = False
 #     while (not terminated and not truncated):
-#         action = pi[state]
+#         action = np.argmax(q[state])
 #         new_state, reward, terminated, truncated, info = env.step(action)
 #         state = new_state
 #     if terminated or truncated:
